8587425e05e12e1d1e624cb4ed8a05b961a71ac2
Lucas Postingher <lucaspostingher@outlook.com>
2024-06-20 14:43:46 -0300

    ELOQUENT API RESOURCES

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,18 @@
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+// use App\Http\Resources\UserCollection;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    return UserResource::make($request->user());
+    // return UserResource::collection(User::all());
+    // return UserResource::collection(User::paginate());
+    // return UserCollection::make(User::all());
+    // return UserCollection::make(User::paginate());
 })->middleware('auth:sanctum');
 
 Route::post('register', RegisterController::class);
